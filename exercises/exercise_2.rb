73c725b15b4af45b2bf5c3afbb643ce8f6c22113
Donnie Walsh <donfather1000@gmail.com>
2021-11-10 03:03:00 +0000

    do exercise 1-5

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+puts @store1
+puts @store2
+
+@store1.name = "GAZTOWNNN xD"
+puts @store1.name
